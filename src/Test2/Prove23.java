4fbdf60c19e392cb78f6cb54254d09ea67312872
DafinaImeraj <dafinaimeraj99@gmail.com>
2019-03-28 15:44:24 +0100

    kjshdfoiuhdwoij

--- src/Test2/Prove23.java
+++ src/Test2/Prove23.java
@@ -5,7 +5,7 @@ public class Prove23 {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("1234567890");
+		System.out.println("1234567890");System.out.println("oijulknkjedskj");
 
 	}
 
