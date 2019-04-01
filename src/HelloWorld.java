6f9b1b99ed31c3e00e95c003c00c38d3ebd12cce
butrintbeqiri <butrintbeqiri@hotmail.com>
2019-04-01 23:20:28 +0200

    JavaFx Text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class HelloWorld
+public class HelloWorld extends Application
 {
+	@Override
+	public void start(Stage primaryStage)
+	{
+		//
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
-		System.out.println("Knk project");
 	}
 }
