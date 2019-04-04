fef989c35dbb02775a0cfbac3a2fdbb4f9c44281
Fatbardh Kadriu <fatbardh.kadriu@gmail.com>
2019-04-04 16:20:58 +0200

    Update databaza.sql

--- sql/databaza.sql
+++ sql/databaza.sql
@@ -11,21 +11,3 @@ psurname varchar(20),
 pdate date ,
 primary key(pid));
 
-create table palidhje(
-nr int primary key);
-
-create table emp(
-eid integer,
-ename varchar(20),
-esurname varchar(20),
-edate date ,
-primary key(eid));
-
-drop table palidhje;
-
-create table palidhje(
-numri int primary key);
-
-create table nota(
-notanepoo integer
-);
