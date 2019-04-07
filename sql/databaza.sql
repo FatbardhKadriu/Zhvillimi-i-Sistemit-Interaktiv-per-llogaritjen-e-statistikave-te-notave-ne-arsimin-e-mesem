a8813d62907291f6629acc56867ec54cf0432145
butrintbeqiri <butrintbeqiri@hotmail.com>
2019-04-07 20:33:11 +0200

    Shtimi i tabeles Parents, Student, Grades

--- sql/databaza.sql
+++ sql/databaza.sql
@@ -44,3 +44,64 @@ CREATE TABLE ClassRoom
 	classRoomNumber VARCHAR(255),
 	PRIMARY KEY(classRoomNumber)
 );
+
+
+#Tabela per te dhenat e prinderve te nxeneseve.
+CREATE TABLE Parents
+(
+	parentID VARCHAR(255),
+	parentName VARCHAR(15),
+	parentSurname VARCHAR(15),
+	parentEmail	VARCHAR(15),
+	parentPhoneNumber INTEGER,
+	parentJob VARCHAR(20),
+	PRIMARY KEY(parentID)
+);
+
+
+#Tabela per studentet (nxenesit)
+CREATE TABLE Student
+(
+	studentID VARCHAR(255),
+    studentName VARCHAR(255),
+    parentID VARCHAR(255),
+    studentSurname VARCHAR(255),
+    gender CHAR(1),
+    phoneNumber VARCHAR(255),
+    email VARCHAR(255),
+    classRoomNumber VARCHAR(255),
+    addressID VARCHAR(255),
+    birthDate DATE,
+    age INTEGER,
+    PRIMARY KEY(studentID),
+    FOREIGN KEY(parentID) REFERENCES Parents(parentID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(classRoomNumber) REFERENCES ClassRoom(classRoomNumber) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(addressID) REFERENCES Address(addressID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+/*
+	Tabela per notat ne nje periode te caktuare. 
+    Vleresimi behet duke llogaritur noten ne dy pjese bazuar ne angazhimet e meposhtme. 
+*/
+CREATE TABLE Grades
+(
+	periodID VARCHAR(255),
+	studentID VARCHAR(255),
+	subjectID VARCHAR(255),
+    commitment REAL,
+    tasks REAL,
+	essay REAL,
+    debates REAL,
+    projects REAL,
+    tests REAL,
+    quizzess REAL,
+	portfolio REAL,
+	markPart1 REAL,
+    finalTest REAL,
+	markPart2 REAL,
+    finalPeriodMark REAL,
+	PRIMARY KEY(periodID),
+	FOREIGN KEY(studentID) REFERENCES Student(studentID) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(subjectID) REFERENCES Subjects(subjectID) ON DELETE CASCADE ON UPDATE CASCADE
+ );
