ad35c2b1040dbb51998a5a122e7f679289006863
MedinaKrelani <medina.krelani@studentet.uni-pr.edu>
2019-05-26 18:51:03 +0200

    klasa Studenti ...vazhdimi

--- src/student/Student.java
+++ src/student/Student.java
@@ -63,5 +63,75 @@ public class Student extends Application{
 		MenuItem itmFind = new MenuItem("Find");
 		MenuItem itmReplace = new MenuItem("Replace");
 		
+		HBox btnPane = new HBox(20);
+		btnPane.setPadding(new Insets(10,10,10,10));
+		btnPane.getChildren().addAll(btnEdit,btnView);
+		StackPane stackBtn = new StackPane();
+		
+		stackBtn.getChildren().add(btnPane);
+		
+		file.getItems().addAll(itmNew , itmSave , itmClose);
+		edit.getItems().addAll(itmCut, itmCopy, itmPaste);
+		subSearch.getItems().addAll(itmFind, itmReplace);
+		
+		
+		menuBar.getMenus().addAll(file,edit,help);
+		edit.getItems().add(subSearch);
+		
+		
+		
+		
+		
+		GridPane ProfilePane = new GridPane();
+		
+		
+		ProfilePane.addRow(0,new Label("First Name : "));
+		ProfilePane.addRow(0, firstName);
+		ProfilePane.addRow(1,new Label("Last Name : "));
+		ProfilePane.addRow(1, lastName);
+		ProfilePane.addRow(2,new Label(" ID : "));
+		ProfilePane.addRow(2, id);
+		ProfilePane.addRow(3,new Label(" Email : "));
+		ProfilePane.addRow(3, email);
+		ProfilePane.addRow(4,new Label("Password : "));
+		ProfilePane.addRow(4, password);
+		ProfilePane.addRow(5,new Label(" Address : "));
+		ProfilePane.addRow(5, address);
+		ProfilePane.addRow(6,new Label("Phone : "));
+		ProfilePane.addRow(6, phone);
+		ProfilePane.setPadding(new Insets(10,10,10,20));
+		
+		VBox pane1 = new VBox(20);
+		profInfo.setStyle("-fx-text-fill: blue;");
+		pane1.getChildren().addAll(profInfo, ProfilePane);
+		HBox btn2Pane = new HBox(20);
+		
+		VBox main = new VBox(30);
+		main.getChildren().addAll(stackBtn,pane1);
+		btn2Pane.getChildren().addAll(btnCancel,btnSave);
+		ProfilePane.add(btn2Pane, 1, 7);
+
+
+		ProfilePane.setVgap(15);
+		ProfilePane.setHgap(15);
+		
+		
+		root.setTop(menuBar);
+		root.setCenter(main);
+		Scene scene = new Scene(root,600,600);
+
+		primaryStage.setTitle("Student");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+
+
+		
 
 }
