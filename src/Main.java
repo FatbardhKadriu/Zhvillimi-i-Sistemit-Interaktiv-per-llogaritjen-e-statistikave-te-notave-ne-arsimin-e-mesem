db514d9f0012babf6f6ca2b871ea64dfe71aad5f
FatbardhGashi <48059724+FatbardhGashi@users.noreply.github.com>
2019-06-12 02:10:46 +0200

    css

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import admin.MainLogin;
@@ -22,6 +24,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage)
 	{
+		
 		Image image1 = new Image("images/admin1.png");
 		Image image2 = new Image("images/teacher1.png");
 		Image image3 = new Image("images/student1.png");
@@ -36,11 +39,11 @@ public class Main extends Application{
 		teacherImage.setFitWidth(100);
 		HBox pane = new HBox(30);
 		admin = new Button("Admin", adminImage);
-			admin.setStyle("-fx-background-color:white;");
+			admin.setStyle("-fx-font-size:15;-fx-background-color:white;-fx-font-weight:bold;-fx-opacity:1;");
 		teacher = new Button("Teacher", teacherImage);
-			teacher.setStyle("-fx-background-color:white;");
+			teacher.setStyle("-fx-font-size:15;-fx-background-color:white;-fx-font-weight:bold;-fx-opacity:1;");
 		student = new Button("Student", studentImage);
-			student.setStyle("-fx-background-color:white;");
+			student.setStyle("-fx-font-size:15;-fx-background-color:white;-fx-font-weight:bold;-fx-opacity:1;");
 		admin.setOnAction(e -> {
 			MainLogin.createStage();
 		});
@@ -55,7 +58,7 @@ public class Main extends Application{
 		pane.getChildren().addAll(admin, teacher, student);
 		pane.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(pane, 600, 300);
-		pane.setStyle("-fx-background-image: url(images/main-background.jpg)");
+			pane.setStyle("-fx-background-image: url(images/main-background.4jpeg.jpeg);-fx-background-position:top;");
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Main");
 		primaryStage.show();
